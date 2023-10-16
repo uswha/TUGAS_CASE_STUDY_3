03b42e4cdac44ef7d5d9916b0c24da70365de362
uswha <uswhanugroho@gmail.com>
2023-10-16 13:25:25 +0700

    update

--- bin/oop_dart.dart
+++ bin/oop_dart.dart
@@ -1,2 +1,59 @@
 import 'package:oop_dart/oop_dart.dart' as oop_dart;
+import 'package:oop_dart/class.dart'; // import library class.dart
 
+
+
+//awal program utama
+void main() {
+  //instansiasi/instance object adalah proses pembuatan object dari sebuah class
+  var programmer = new manusia('Fauko misalam'); // membuat object dari class manusia 
+  programmer.email ='fauko@bsi.ac.id';
+  programmer.info();
+  programmer.makan(); // objek programer memanggil method makan di class manusia
+  print(programmer.nama);
+  print(programmer.email);
+  print(programmer.password); //akses atribut_password
+
+} // akhir program utama
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//program utama
+void main(){
+  //instansiasi/instance object adalah proses pembuatan object dari sebuah class
+  var programmer = new ManusiaMilenial('Fauko Misalam');
+  programmer.email = 'fauko@bsi.ac.id';
+  programmer.info();
+  programmer.makan();
+
+  print('\n'); // pindah baris
+
+  var dosen = new ManusiaMilenial('intan');
+  dosen.email = 'intan@bsi.ac.id';
+  dosen.info();
+  dosen.makan();
+
+  print('\n'); // pindah baris
+
+  var hacker = new ManusiaMilenial('Dio');
+  hacker.email = 'dio@bsi.ac.id';
+  hacker.info();
+  hacker.makan();
+}
--- lib/class.dart
+++ lib/class.dart
@@ -2,9 +2,29 @@ class manusia {
   //deklarasi atribut /properti 
   string nama = '';//atribut nama bernilai string dan nilai awal null
 
+//pembuatan konstruktor, nama konstruksor adalah nama class
+Manusia(nm){
+  // nm a/ sebuah parameter yang dipergunakan untuk menampung data
+  this.nama = nm; // atribut nama memiliki data dari paarameter nm
+}
 
   //deklarasi method/fungsi/behavior/ bernama makan
   void makan(){
-    print('makan nasi');
+    print('$nama makan nasi');
   }
 }
+
+//awal inheritance
+class ManusiaMilenial extends Manusia {
+  string email = '';
+
+
+  ManusiaMilenial(string email) : super(email);
+  void info(){
+    print('nama: $nama, Email: $email');
+  }
+} //akhir inheritance
+
+
+
+
